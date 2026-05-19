53c941997fef67d2b06d64dc9eccf52f93323b3c
iswat <ibeetek@gmail.com>
2026-05-19 16:26:43 +0100

    refactor: Clean up code formatting and update README documentation

--- chat-app/frontend/script.js
+++ chat-app/frontend/script.js
@@ -26,34 +26,30 @@ const formElement = document.getElementById("chat-form");
 const senderElement = document.getElementById("chat-sender");
 const messageElement = document.getElementById("chat-message");
 
+formElement.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
+  const senderValue = senderElement.value;
+  const messageValue = messageElement.value;
 
-formElement.addEventListener("submit", async(event) => {
-    event.preventDefault();
+  // Send the data
+  const response = await fetch("http://localhost:3000/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      sender: senderValue,
+      text: messageValue,
+    }),
+  });
 
-    const senderValue = senderElement.value;
-    const messageValue = messageElement.value;
+  if (response.ok) {
+    // refresh the list of messages
+    getAllMessages();
 
-    // Send the data
-    const response = await fetch("http://localhost:3000/messages", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            sender: senderValue,
-            text: messageValue
-        })
-    });
-
-    if (response.ok) {
-        // refresh the list of messages
-        getAllMessages();
-
-        // clear the sender and message input values
-        senderElement.value = "";
-        messageElement.value = "";
-        
-    }
-
-} )
+    // clear the sender and message input values
+    senderElement.value = "";
+    messageElement.value = "";
+  }
+});
